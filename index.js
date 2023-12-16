2519d788e1a0f79bed7b3a9691992ff50fd6524f
RobinTheRedLight <robinmitra789@gmail.com>
2023-12-16 15:00:43 +0600

    cart route added

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fghxosh.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -22,11 +21,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
     const menuCollection = client.db("restaurantDB").collection("menu");
     const reviewsCollection = client.db("restaurantDB").collection("reviews");
+    const cartCollection = client.db("restaurantDB").collection("cart");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -36,15 +35,18 @@ async function run() {
       const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
+    app.post("/cart", async (req, res) => {
+      const item = req.body;
+      console.log(item);
+      const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
 
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
   }
 }
 run().catch(console.dir);
