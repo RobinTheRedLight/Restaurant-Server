3d8609c0c3c4d2a615707a3d8183c46f0f30f9d3
RobinTheRedLight <robinmitra789@gmail.com>
2024-02-01 23:38:57 +0600

    Vercel file added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
-//middleware
+//middleware;
 app.use(cors());
 app.use(express.json());
 
